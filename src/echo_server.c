8c67940815c949c1d39946fcdf26fd57a1afb8e5
MarioR17 <jmrendonq17@gmail.com>
2025-07-19 23:06:29 -0400

    Initial commit

---
+++ src/echo_server.c
@@ -0,0 +1,36 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+
+#define PORT "8080"
+
+int main(int argc, char *argv[])
+{
+    (void) argv[argc-1];
+    int status;
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(EXIT_FAILURE);
+    } else {
+        fprintf(stdin, "getaddrinfo success\n");
+    }
+
+    freeaddrinfo(servinfo);
+
+    return 0;
+}
+
+
